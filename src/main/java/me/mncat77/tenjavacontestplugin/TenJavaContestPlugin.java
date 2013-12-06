c7cdb161fbafb1f9a03ac1a23458b1d0cd8c6be1
mncat77 <jo-97@web.de>
2013-12-06 15:50:13 +0100

    Initial Commit - Plugin Skeleton

---
+++ src/main/java/me/mncat77/tenjavacontestplugin/TenJavaContestPlugin.java
@@ -0,0 +1,11 @@
+package me.mncat77.tenjavacontestplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TenJavaContestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
+
